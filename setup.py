438ac99c2b30837ab4e1f4bd323fee215d20dc73
ashwith-atluri <ashwithatluri@gmail.com>
2020-04-18 20:54:22 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="RBHC",
     packages=['RBHC'],
-    version="1.0.0",
+    version="1.0.1",
     author="Ashwith Atluri",
     author_email="ashwithatluri@gmail.com",
     description="RBHC",
